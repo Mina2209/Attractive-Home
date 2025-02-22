f4181e5b94e9df3bea6ab8647ced02bc32b9a253
Mina2209 <mina.nassef09@gmail.com>
2025-02-22 14:00:14 +0200

    Add hover play functionality to VideoPlayer component

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -7,11 +7,14 @@ const VideoPlayer = ({
   videoUrl,
   className = "",
   setLoading,
+  autoPlay = true,
   defaultMuted = true,
   showMuteButton = true,
+  enableHoverPlay = false,
 }) => {
   const videoRef = useRef(null);
   const [isMuted, setIsMuted] = useState(defaultMuted);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
     const video = videoRef.current;
@@ -41,6 +44,21 @@ const VideoPlayer = ({
     setIsMuted((prevMuted) => !prevMuted);
   };
 
+  const handleMouseEnter = (e) => {
+    if (enableHoverPlay) {
+      setIsPlaying(true);
+      e.target.play();
+    }
+  };
+
+  const handleMouseLeave = (e) => {
+    if (enableHoverPlay) {
+      setIsPlaying(false);
+      e.target.pause();
+      e.target.currentTime = 0;
+    }
+  };
+
   const isAbsolute = className.includes("absolute");
 
   return (
@@ -48,24 +66,20 @@ const VideoPlayer = ({
       <video
         className={className}
         ref={videoRef}
-        autoPlay
+        autoPlay={autoPlay}
         loop
         muted={isMuted}
         playsInline
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       />
       {showMuteButton && (
         <button
           onClick={toggleMute}
           className="absolute right-0 bottom-3/4 transform -translate-y-3/4 bg-black bg-opacity-10 text-white px-3 py-1 rounded-l-lg text-sm z-10 [@media(max-height:750px)]:bottom-2/3 [@media(max-height:750px)]:-translate-y-2/3
           [@media(max-height:750px)]:bottom-[60%] 
-          [@media(max-height:750px)]:-translate-y-[60%]" //gap-2 flex
-          // className="absolute left-0 bottom-11 bg-black bg-opacity-30 text-white px-3 py-1 rounded-r-lg text-sm z-10"
+          [@media(max-height:750px)]:-translate-y-[60%]"
         >
-          {/* <FontAwesomeIcon
-            icon={isMuted ? faVolumeUp : faVolumeMute}
-            className="text-lg"
-          />
-          <span className="font-medium">{isMuted ? "Unmute" : "Mute"}</span> */}
           {isMuted ? "Unmute 🔊" : "Mute 🔇"}
         </button>
       )}
