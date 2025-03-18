2aafee3fb8069097d628622099f4e2079a5edb02
pola2555 <polamichile25@gmail.com>
2025-03-18 19:23:23 +0200

    Enhance VideoPlayer component to support autoplay behavior for Instagram browser and improve video loading handling

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -16,6 +16,7 @@ const VideoPlayer = ({
   useEffect(() => {
     const video = videoRef.current;
 
+    // Set up HLS or direct video source
     if (Hls.isSupported()) {
       const hls = new Hls();
       hls.loadSource(videoUrl);
@@ -23,7 +24,20 @@ const VideoPlayer = ({
     } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
       video.src = videoUrl;
     }
-  }, [videoUrl]);
+
+    // If autoplay is false, force display of the first frame.
+    if (!autoPlay) {
+      const handleLoadedData = () => {
+        // Set a slight offset to ensure a frame is rendered.
+        video.currentTime = 0.1;
+      };
+
+      video.addEventListener("loadeddata", handleLoadedData);
+      return () => {
+        video.removeEventListener("loadeddata", handleLoadedData);
+      };
+    }
+  }, [videoUrl, autoPlay]);
 
   const toggleMute = () => {
     setIsMuted((prevMuted) => !prevMuted);
@@ -61,9 +75,7 @@ const VideoPlayer = ({
       {showMuteButton && (
         <button
           onClick={toggleMute}
-          className="absolute right-0 bottom-3/4 transform -translate-y-3/4 bg-black bg-opacity-10 text-white px-3 py-1 rounded-l-lg text-sm z-10 [@media(max-height:750px)]:bottom-2/3 [@media(max-height:750px)]:-translate-y-2/3
-          [@media(max-height:750px)]:bottom-[60%] 
-          [@media(max-height:750px)]:-translate-y-[60%]"
+          className="absolute right-0 bottom-3/4 transform -translate-y-3/4 bg-black bg-opacity-10 text-white px-3 py-1 rounded-l-lg text-sm z-10 [@media(max-height:750px)]:bottom-2/3 [@media(max-height:750px)]:-translate-y-2/3 [@media(max-height:750px)]:bottom-[60%] [@media(max-height:750px)]:-translate-y-[60%]"
         >
           {isMuted ? "Unmute 🔊" : "Mute 🔇"}
         </button>
