f1ceeca128a8d1d1452d42e7f4a4cfa440c1e62c
Mina2209 <mina.nassef09@gmail.com>
2025-04-02 16:27:45 +0200

    Add new sections

--- client/src/components/AluminumSkirting.jsx
+++ client/src/components/AluminumSkirting.jsx
@@ -2,24 +2,24 @@ const AluminumSkirting = () => {
   return (
     <section>
       <div
-        className="relative flex flex-col lg:justify-end min-h-screen bg-cover bg-fixed bg-center text-white lg:px-32 max-lg:text-center max-lg:items-center"
+        className="relative flex flex-col lg:justify-end min-h-screen bg-cover bg-center text-white lg:px-32 max-lg:text-center max-lg:items-center"
         style={{
-          backgroundImage: "url('Aluminum-Skirting/Background.avif')",
+          backgroundImage: "url('/Aluminum-Skirting/Background.avif')",
         }}
       >
         <div className="lg:hidden" style={{ paddingTop: "21vh" }}></div>
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
-        <div className="relative z-10 max-w-4xl">
+        <div className="relative z-10 max-w-4xl 2xl:max-w-5xl">
           <h1
-            className="text-6xl max-sm:text-4xl font-bold tracking-[1.6px] max-sm:max-w-xs mx-auto lg:mx-0"
+            className="text-6xl 2xl:text-7xl max-sm:text-4xl font-bold tracking-[1.6px] max-sm:max-w-xs mx-auto lg:mx-0"
             style={{ lineHeight: "1.5" }}
           >
             Transformative Aluminum Skirting Profiles
           </h1>
           <div className="lg:hidden" style={{ paddingBottom: "24vh" }}></div>
           <p
-            className="mt-3 text-md sm:text-xl tracking-[0.7px] md:max-w-2xl lg:max-w-2xl max-sm:px-8 mx-auto lg:mx-0"
+            className="mt-3 text-md sm:text-xl 2xl:text-2xl tracking-[0.7px] md:max-w-2xl 2xl:max-w-3xl lg:max-w-2xl max-sm:px-8 mx-auto lg:mx-0"
             style={{ lineHeight: "1.4" }}
           >
             Elevate your space with sleek, modern aluminum profiles that blend
@@ -29,14 +29,14 @@ const AluminumSkirting = () => {
             href="https://s3.me-central-1.amazonaws.com/attractivehome.ae/Aluminum-Skirting/ATT-001-Guide.pdf"
             target="_blank"
             rel="noopener noreferrer"
-            className="mt-8 mb-20 inline-block px-14 py-4 text-lg font-medium text-gray-900 bg-white rounded-full hover:bg-gray-200"
+            className="mt-8 mb-20 inline-block px-14 py-4 text-lg 2xl:text-xl font-medium text-gray-900 bg-white rounded-full hover:bg-gray-200"
           >
             Discover
           </a>
         </div>
       </div>
 
-      <div className="container mx-auto px-10 py-24 bg-[#fafafa]">
+      <div className="mx-auto px-10 2xl:px-20 py-24 bg-[#fafafa]">
         <h2 className="text-3xl sm:text-4xl font-bold text-center text-gray-900 mb-6">
           Premium Aluminum Skirting Profiles
         </h2>
@@ -46,7 +46,7 @@ const AluminumSkirting = () => {
         </p>
         <div className="flex flex-wrap items-center">
           <img
-            src="Aluminum-Skirting/Image1-Desk.jpg"
+            src="/Aluminum-Skirting/Image1-Desk.jpg"
             alt="Aluminum Skirting"
             className="w-full lg:w-1/2 rounded-2xl shadow-lg hidden lg:block"
           />
@@ -88,10 +88,184 @@ const AluminumSkirting = () => {
               </p>
             </div>
             <img
-              src="Aluminum-Skirting/Image1-Mob.avif"
+              src="/Aluminum-Skirting/Image1-Mob.avif"
               alt="Aluminum Skirting"
               className="w-full lg:w-1/2 rounded-2xl shadow-lg mt-16 block lg:hidden"
             />
+          </div>
+        </div>
+      </div>
+
+      {/* Sleek Profiles Section */}
+      <div className="mx-auto px-10 2xl:px-20 py-24">
+        <h2 className="text-3xl sm:text-4xl font-bold text-center text-gray-900 mb-6">
+          Sleek Profiles
+        </h2>
+        <p className="text-center text-gray-600 mb-14">
+          Transformative power, clean lines, modern touch, flawless look,
+          impeccable clean
+        </p>
+        <div className="flex flex-wrap justify-between gap-8">
+          <img
+            src="/Aluminum-Skirting/Sleek1.avif"
+            alt="Sleek Profile 1"
+            className="w-[45%] lg:w-1/5 rounded-lg shadow-lg"
+          />
+          <img
+            src="/Aluminum-Skirting/Sleek2.avif"
+            alt="Sleek Profile 2"
+            className="w-[45%] lg:w-1/5 rounded-lg shadow-lg"
+          />
+          <img
+            src="/Aluminum-Skirting/Sleek3.avif"
+            alt="Sleek Profile 3"
+            className="w-[45%] lg:w-1/5 rounded-lg shadow-lg"
+          />
+          <img
+            src="/Aluminum-Skirting/Sleek4.avif"
+            alt="Sleek Profile 4"
+            className="w-[45%] lg:w-1/5 rounded-lg shadow-lg"
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col lg:flex-row bg-white py-24 px-10 2xl:px-20 text-gray-900 gap-20 mx-auto">
+        {/* Left side - Product image */}
+        <div className="lg:w-1/3">
+          <div>
+            <img
+              src="/Aluminum-Skirting/ATT-001.avif"
+              alt="ATT-001 Aluminum Profile"
+              className="w-full object-cover"
+            />
+          </div>
+
+          <div className="bg-gray-900 text-white p-6">
+            <h2 className="text-2xl font-bold mb-4">
+              Aluminum Profile ATT-001
+            </h2>
+            <p className="mb-2">
+              Durable, stylish, versatile aluminum profile for projects.
+            </p>
+            <p className="text-yellow-500">Plastered Walls Profile</p>
+          </div>
+        </div>
+
+        {/* Right side - Product description */}
+        <div className="lg:w-2/3">
+          <h1 className="text-2xl font-bold mb-2">
+            ATT-001 - Precision for Plastered Walls
+          </h1>
+
+          <p className="mb-4">
+            Designed for <span className="font-bold">plastered walls</span>, the{" "}
+            <strong>ATT-001</strong> aluminum profile seamlessly creates a{" "}
+            <span className="font-bold">2 cm shadow gap</span> between the walls
+            and flooring, adding a sleek and modern aesthetic. This profile has
+            been successfully applied in numerous projects, offering durability
+            and precision. To ensure optimal performance, it should be installed
+            following the provided installation guide.{" "}
+            <span className="font-bold">ATT-001</span> enhances the transition
+            between surfaces, making it an ideal choice for architects and
+            designers aiming for a refined, seamless look.
+          </p>
+
+          {/* Arabic text section */}
+          <p className="mb-8 text-right" dir="rtl" lang="ar">
+            تم تصميم <span className="font-bold">ATT-001</span> خصيصًا{" "}
+            <strong>للجدران الممحرة (محارة)</strong>، حيث يوفر
+            <span className="font-bold"> فجوة ظل بعرض 2 سم</span> بين الجدران
+            والأرضية، مما يضفي مظهراً أنيقاً وعصرياً. تم تطبيق هذا الملف الشخصي
+            بنجاح في العديد من المشاريع، مما يثبت متانته ودقته في التصميم لضمان
+            الأداء الأمثل، يجب تثبيته وفقًا لدليل التركيب المرفق. يعزز{" "}
+            <span className="font-bold">ATT-001</span> الانتقال السلس بين
+            الأسطح، مما يجعله خيارًا مثاليًا للمهندسين المعماريين والمصممين
+            الذين يسعون للحصول على تشطيب متقن ومتناسق.
+          </p>
+
+          {/* Button section */}
+          <div className="flex flex-wrap gap-4">
+            <a
+              href="https://s3.me-central-1.amazonaws.com/attractivehome.ae/ATT-001-DIMS.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="bg-black hover:bg-gray-800 text-white font-medium py-3 px-6 rounded-full">
+                Dimensions
+              </button>
+            </a>
+            <a
+              href="https://s3.me-central-1.amazonaws.com/attractivehome.ae/Aluminum-Skirting/ATT-001-Guide.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="bg-black hover:bg-gray-800 text-white font-medium py-3 px-6 rounded-full">
+                Technical Guide
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col lg:flex-row bg-white py-24 px-10 2xl:px-20 text-gray-900 gap-20 mx-auto">
+        {/* Left side - Product image */}
+        <div className="lg:w-1/3">
+          <div>
+            <img
+              src="/Aluminum-Skirting/ATT-002.avif"
+              alt="ATT-002 Aluminum Profile"
+              className="w-full object-cover"
+            />
+          </div>
+
+          <div className="bg-gray-900 text-white p-6">
+            <h2 className="text-2xl font-bold mb-4">
+              Aluminum Profile ATT-002
+            </h2>
+            <p className="text-yellow-500">Wall Claddings Profile</p>
+          </div>
+        </div>
+
+        {/* Right side - Product description */}
+        <div className="lg:w-2/3">
+          <h1 className="text-2xl font-bold mb-2">
+            ATT-002 - Perfect for Claddings (All types)
+          </h1>
+
+          <p className="mb-4">
+            Complementing <strong>ATT-001</strong>, the <strong>ATT-002</strong>{" "}
+            aluminum profile is designed for{" "}
+            <strong>wood and marble claddings.</strong> It also creates a{" "}
+            <strong>2 cm shadow gap</strong>, ensuring a cohesive and
+            sophisticated finish across different surfaces. Whether used in wall
+            paneling or decorative cladding, <strong>ATT-002</strong> maintains
+            a consistent design language, making it an essential component for
+            high-end interior and architectural detailing. Together,{" "}
+            <strong>ATT-001</strong> and <strong>ATT-002</strong>
+            provide a complete solution for achieving clean lines and
+            contemporary elegance.
+          </p>
+
+          {/* Arabic text section */}
+          <p className="mb-8 text-right" dir="rtl" lang="ar">
+            يكمل <strong>ATT-001</strong>، حيث تم تصميم <strong>ATT-002</strong>{" "}
+            خصيصًا<strong> لتكسية الخشب والرخام</strong>. يوفر أيضًا{" "}
+            <span className="font-bold"> فجوة ظل بعرض 2 سم</span>، مما يضمن
+            تشطيبًا متناسقًا وأنيقًا عبر مختلف الأسطح. سواء تم استخدامه في
+            الألواح الجدارية أو التكسية الديكورية، يحافظ{" "}
+            <strong>ATT-002</strong> على لغة تصميم موحدة، مما يجعله عنصرًا
+            أساسيًا في التشطيبات الداخلية والهندسة المعمارية الراقية. معًا، يوفر
+            <strong>ATT-001</strong> و<strong>ATT-002</strong> حلاً متكاملاً
+            للحصول على خطوط نظيفة وأناقة عصرية.
+          </p>
+
+          {/* Button section */}
+          <div className="flex flex-wrap gap-4">
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <button className="bg-black hover:bg-gray-800 text-white font-medium py-3 px-6 rounded-full">
+                Dimensions
+              </button>
+            </a>
           </div>
         </div>
       </div>
